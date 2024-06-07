2b07b3fd67c4011bed3c24e9df9093a695c1c948
Tim Heilman <tdh@stanfordalumni.org>
2024-06-07 12:19:52 -0700

    test: hopefully minimal implementation to reproduce issue

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
+import { getItemAsync, setItemAsync } from 'expo-secure-store';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const exampleKey = "myExampleKey";
+  const exampleValue = "myExampleValue";
+  useEffect(() => {
+    getItemAsync(exampleKey).then((exampleValue) => {
+      console.log(`Example value: ${exampleValue}`)
+    }).catch((error) => {
+      // This alert demonstrates the error reproduction:
+      alert(error.message);
+    });
+    setItemAsync(exampleKey, exampleValue).then(()=> {
+      console.log("Set successfully!");
+    }).catch((error) => {
+      console.error("Unexpected error on set");
+      console.error(error);
+    });
+  });
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Run this once, make a small change, rebuild, and run again to get the error reproduced.</Text>
       <StatusBar style="auto" />
     </View>
   );
